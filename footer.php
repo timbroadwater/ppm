8dd33e69ba65e7c13e4c2a9b54bdf0821828f370
Tim Broadwater <tim.broadwater@leidos.com>
2019-06-03 14:49:09 -0400

    Completed Footer

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
-	</div>
-		<footer id="footer">
+</div>
+	<footer id="footer">
+		<div id="wrapper">
 			<div id="footer-sidebar1">
 				<?php
 				if(is_active_sidebar('footer-sidebar-1')){
@@ -22,11 +23,11 @@
 				?>
 			</div>
 			<div id="copyright">
-				<p>Copyright The Pantry Plus More, Inc. © 2019 </p>
+				<p>Copyright The Pantry Plus More, Inc. <nobr>© 2019</nobr></p>
 			<!-- &copy; <?php echo esc_html( date_i18n( __( 'Y', 'ppm2019' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?> -->
 			</div>
-		</footer>
-	</div>
+		</div>
+	</footer>
 	<?php wp_footer(); ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -6,12 +6,13 @@
 
 	<!-- Imported CSS -->
 	<link href="https://fonts.googleapis.com/css?family=Karla:400,400i,700,700i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&display=swap" rel="stylesheet">
+	<link href="https://netdna.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.css" type="text/css" rel="stylesheet">
 
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-	<div id="wrapper" class="hfeed">
-		<header id="header">
+	<header id="header">
+		<div id="wrapper" class="hfeed">	
 			<div id="branding">
 				<a href="/"><img alt="Pantry Plus More Logo" title="Pantry Plus More" src="<?php echo get_bloginfo('template_url') ?>/images/logo.png"/></a>
 				<!--  <div id="site-title">
@@ -25,5 +26,6 @@
 				<!--<div id="search"><?php get_search_form(); ?></div> -->
 				<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
 			</nav>
-		</header>
-		<div id="container">
+		</div>
+	</header>
+	<div id="container">
