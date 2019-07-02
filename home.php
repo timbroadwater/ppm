f42809a9816b641f7067aadec2bbb9d041d90ab0
Tim Broadwater <tim.broadwater@leidos.com>
2019-07-02 14:35:25 -0400

    Finished Homepage Template

--- home.php
+++ home.php
@@ -28,23 +28,24 @@
 </main> -->
 
 <!-- Impact Card -->
-<div class="colwrapper">
+<div class="colwrapper flexing">
 	<div class="twocol twocol-textpadleft">
-		<h2>Our Impact</h2>
+		<hr>
+		<h1>Our Impact</h1>
 		<p>The Pantry Plus More is a non-profit organization that operates free school pantries, food giveaways, and health and wellness programs across Monongalia County.<br><br><b>Since 2016, we have…</b></p>
 	</div>
 	<div class="twocol">
-		<img alt="Pantry Plus More" title="Pantry Plus More" src="<?php echo get_bloginfo('template_url') ?>/images/header5.jpg"/>
+		<img alt="Pantry Plus More" title="Pantry Plus More" src="<?php echo get_bloginfo('template_url') ?>/images/header6.jpg"/>
 	</div>
 </div>
 
 <!-- Results-->
 <div class="colwrapper">
-	<div class="onecol">
+	<div id="results" class="onecol">
 		<ul>
 			<li>Reached 1,200 students every month</li>
-			<li>Reached 1,200 students every month</li>
-			<li>Reached 1,200 students every month</li>
+			<li>Distributed over 350,000 lbs of food to families in need</li>
+			<li>Touched the lives of tens of thousands of West Virginians</li>
 		</ul>
 	</div>
 </div>
@@ -52,8 +53,11 @@
 <!-- Get Involved Stories-->
 <div class="colwrapper">
 	<div class="onecol">
-		<h2>Get Involved</h2>
-		<p>As an all-volunteer organization, The Pantry Plus More is always looking for passionate people to help our mission. There are multiple ways to get involved:</p>
+		<span class="centertext">
+			<hr>
+			<h1>Get Involved</h1>
+			<p>As an all-volunteer organization, The Pantry Plus More is always looking for passionate people to help our mission. There are multiple ways to get involved:</p>
+		</span>
 	</div>
 </div>
 
@@ -61,58 +65,62 @@
 <div class="colwrapper">
 	<div class="threecol">
 		<img alt="" title="" src="<?php echo get_bloginfo('template_url') ?>/images/mobile-food-giveaway.svg"/>
-		<h3>Mobile Food Giveaway</h3>
+		<h2>Mobile Food Giveaway</h2>
 		<p>A partnership with Mountaineer Food Bank, this food giveaway distributes more than 12,000 lbs of foods to people in need. These monthly, Saturday morning events typically require 60 volunteers who are capable of lifting 40-lb food boxes and are committed to staying for the entire 4-hour event.</p>
 	</div>
 	<div class="threecol">
 		<img alt="" title="" src="<?php echo get_bloginfo('template_url') ?>/images/health-wellness.svg"/>
-		<h3>Fresh Start Health & Wellness</h3>
+		<h2>Fresh Start Health & Wellness</h2>
 		<p>This program provides 8,000 lbs of fresh food to families in need every month. 10-15 volunteers are needed for these events, which occur at 8:30 a.m. on the second Thursday of every month.</p>
 	</div>
-	<div class="threecol">
+	<div class="threecol clearer">
 		<img alt="" title="" src="<?php echo get_bloginfo('template_url') ?>/images/school-pantries.svg"/>
-		<h3>In-School Pantries</h3>
+		<h2>In-School Pantries</h2>
 		<p>Our in-school pantries operate in partnership with volunteer school representatives, who are liaisons between between the schools and The Pantry Plus More. The hands-on training for these selective volunteer positions typically lasts six months. In addition, we also need 5-15 volunteers to help with our weekly inventory checks, which take place on weekends.</p>
 	</div>
 </div>
 
 <!-- Volunteer-->
 <div class="colwrapper">
-	<div class="onecol">
-		<button>Volunteer</button>
+	<div class="onecol center">
+		<button onclick="window.location.href = '../get-involved/';" class="primary">Volunteer</button>
 	</div>
 </div>
 
 <!-- Facebook Stories-->
-<div class="colwrapper">
-	<div class="onecol"></div>
+<div class="colwrapperbig">
+	<div class="onecol">
+		<h1> Latest Stories</h1>
+	</div>
 </div>
 
 <!-- Hiring-->
-<div class="colwrapper">
+<div class="colwrapperbig">
 	<div class="twocol twocol-textpadleft">
-		<h2>Our Board is always looking  for people with a passion for helping those in need.</h2>
+		<hr>
+		<h1>Our Board is always looking  for <span class="red">people with a passion</span> for helping those in need.</h1>
 	</div>
 	<div class="twocol">
-		<p><b>We currently have three open Board Member positions:</b></p>
-		<ul>
-			<li>At-large Board Member Position One</li>
-			<li>At-large Board Member Position Two</li>
-			<li>Fundraising Co-Chair</li>
-		</ul>
-		<button>Apply</button>
+		<h2>We currently have three open Board Member positions:</h2>
+			<ul>
+				<li>At-large Board Member Position One</li>
+				<li>At-large Board Member Position Two</li>
+				<li>Fundraising Co-Chair</li>
+			</ul>
+		<button onclick="window.location.href = '../get-involved/';" class="secondary">Apply</button>
 	</div>
 </div>
 
 <!-- Donate Card -->
-<div class="colwrapper">
+<div class="colwrapperbig flexing">
 	<div class="twocol">
-		<img alt="Pantry Plus More" title="Pantry Plus More" src="<?php echo get_bloginfo('template_url') ?>/images/header4.jpg"/>
+		<img alt="Pantry Plus More" title="Pantry Plus More" src="<?php echo get_bloginfo('template_url') ?>/images/header5.jpg"/>
 	</div>
 	<div class="twocol twocol-textpadright">
-		<h2>Donate</h2>
+		<hr>
+		<h1>Donate</h1>
 		<p>The Pantry Plus More is proud to operate with some of the best efficiency in our sector. As a volunteer organization, X% of your tax-deductible donation is allocated to our programs that provide nutrition, hygiene, and school supplies to children and families in need, with a small portion of donations supporting our operating costs.</p><br>
-		<button>Donate</button>
+		<button onclick="window.location.href = '../get-involved/';" class="primary">Donate</button>
 	</div>
 </div>
 
